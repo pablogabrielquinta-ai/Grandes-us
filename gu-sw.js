a56ebef4c67e7092fbf0643eb6f634f91e5b64f8
pablogabrielquinta-ai <pablogabrielquinta@gmail.com>
2026-07-27 14:10:22 -0300

    Add files via upload

--- gu-sw.js
+++ gu-sw.js
@@ -1,7 +1,7 @@
 /* DPEC GU Campo - service worker
    Estrategia: red primero, caché de respaldo. */
 
-var CACHE = 'dpec-gu-campo-v1';
+var CACHE = 'dpec-gu-campo-v2';
 var ARCHIVOS = [
   './dpec_gu_campo.html',
   './gu-manifest.json',
