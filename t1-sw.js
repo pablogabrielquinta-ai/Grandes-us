60aa1a15d316c815640f157be7bc461aad45dd49
pablogabrielquinta-ai <pablogabrielquinta@gmail.com>
2026-08-27 12:17:03 -0300

    Add files via upload

---
+++ t1-sw.js
@@ -0,0 +1,35 @@
+var CACHE_NAME = "dpec-gu-t1-v1";
+var FILES_TO_CACHE = [
+  "./dpec_gu_t1-1.html",
+  "./gu-icon-192.png",
+  "./gu-icon-512.png"
+];
+
+self.addEventListener("install", function (event) {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(
+        keys.filter(function (key) { return key !== CACHE_NAME; })
+            .map(function (key) { return caches.delete(key); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (cached) {
+      return cached || fetch(event.request);
+    })
+  );
+});
